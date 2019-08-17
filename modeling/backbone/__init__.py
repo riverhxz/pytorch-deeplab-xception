1c7d932d8143e60d81e5306634a6eeb50b530f06
riverhxz <ravehun@qq.com>
2019-08-17 21:14:16 +0800

    add pretrained param

--- modeling/backbone/__init__.py
+++ modeling/backbone/__init__.py
@@ -1,13 +1,13 @@
 from modeling.backbone import resnet, xception, drn, mobilenet
 
-def build_backbone(backbone, output_stride, BatchNorm):
+def build_backbone(backbone, output_stride, BatchNorm, pretrained=True):
     if backbone == 'resnet':
-        return resnet.ResNet101(output_stride, BatchNorm)
+        return resnet.ResNet101(output_stride, BatchNorm,pretrained)
     elif backbone == 'xception':
-        return xception.AlignedXception(output_stride, BatchNorm)
+        return xception.AlignedXception(output_stride, BatchNorm,pretrained)
     elif backbone == 'drn':
-        return drn.drn_d_54(BatchNorm)
+        return drn.drn_d_54(BatchNorm,pretrained)
     elif backbone == 'mobilenet':
-        return mobilenet.MobileNetV2(output_stride, BatchNorm)
+        return mobilenet.MobileNetV2(output_stride, BatchNorm,pretrained)
     else:
         raise NotImplementedError
